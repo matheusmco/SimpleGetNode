12bf8b53dbf1744969a1f7ab85c4a47f3fe2767a
Matheus Oliveira <matheus0mco@gmail.com>
2021-04-02 14:25:05 -0300

    feat:getting return object from sqlite

--- api/controllers/mainController.js
+++ api/controllers/mainController.js
@@ -1,5 +1,18 @@
 'use strict';
 
-exports.get = function(req, res) {
-    res.json({"data":"hello world"});
+const sqlite3 = require('sqlite3').verbose();
+
+exports.get = function (req, res) {
+    let db = new sqlite3.Database('/home/matheus/repos/SimpleGetNode/api/base.db');
+
+    let return_data = {}
+
+    db.all('SELECT * FROM api_data', [], (err, rows) => {
+        rows.forEach((row) => {
+            return_data[row.key] = row.data
+            res.json(return_data);
+        });
+    });
+
+    db.close();
 };
---
+++ build.sh
@@ -0,0 +1,5 @@
+# install node
+# install npm packages
+# install sqlite3
+# run sqlite3 to create table and insert data
+# run npm run start
